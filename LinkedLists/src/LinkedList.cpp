b4c4a8913fa46c25cab937ca6cc7cd6b35d39c78
Caleb O'Neal <caleboneal07@gmail.com>
2025-02-03 09:27:06 -0500

    Initial linked list tests implemented

--- LinkedLists/src/LinkedList.cpp
+++ LinkedLists/src/LinkedList.cpp
@@ -1,20 +1,55 @@
 #include "LinkedList.h"
+
 using namespace std;
 
-Node::Node()
-{
+Node::Node() {
     cargo = 0;
     next = nullptr;
 }
 
-Node::Node(int cargo)
-{
+Node::Node(int cargo) {
     this->cargo = cargo;
     next = nullptr;
 }
 
-Node::Node(int cargo, Node* next)
-{
+Node::Node(int cargo, Node* next) {
     this->cargo = cargo;
     this->next = next;
+}
+
+string Node::to_string() {
+        return std::to_string(this->cargo);
+}
+
+string render_list(Node *list) {
+    Node* node = list;
+    string s = "";
+    while (node != nullptr) {
+        s += node->to_string();
+        node = node->next;
+        if (node != nullptr)
+           s += ", ";
+    }
+    return s;
+}
+
+string render_list_backward(Node* list) {
+    return render_backward_worker(list, "");
+}
+
+string render_backward_worker(Node *list, string s) {
+    if (list == nullptr) return "";
+    Node* head = list;
+    Node* tail = list->next;
+
+    s = render_backward_worker(tail, s) + s;
+    if (head->next != nullptr)
+        s += ", ";
+    s += head->to_string();
+
+    return s;
+}
+
+string render_pretty(Node* list, string (*list_renderer)(Node*)) {
+    return "(" + list_renderer(list) + ")";
 }
--- LinkedLists/src/LinkedList.h
+++ LinkedLists/src/LinkedList.h
@@ -1,5 +1,7 @@
 using namespace std;
 
+#include <string>
+
 struct Node {
     int cargo;
     Node* next;
@@ -8,4 +10,12 @@ struct Node {
     Node();
     Node(int);
     Node(int, Node*);
+
+    // Methods
+    string to_string();
 };
+
+string render_list(Node*);
+string render_list_backward(Node*);
+string render_backward_worker(Node*, string="");
+string render_pretty(Node*, string (*)(Node*));
